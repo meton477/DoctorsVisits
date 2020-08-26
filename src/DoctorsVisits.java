41289b8aed878972843f40ce0dc337546870e42f
Meton477 <regdload@yandex.com>
2020-08-26 08:35:52 -0700

    DocOfficeNoDigits

---
+++ src/DoctorsVisits.java
@@ -0,0 +1,15 @@
+public class DoctorsVisits {
+    public static void main(String[] args)
+    {
+        String patiantName = "Stacey Manson";
+        boolean hasInsurance = false;
+        double temp = 102.2;
+        int numVisits = 5;
+
+        System.out.println("Patiant Ditails:");
+        System.out.println("Name: " + patiantName);
+        System.out.println("Patiant has Insurance: " + hasInsurance);
+        System.out.println("Body temrature: ");
+        System.out.println("Number of visits: " );
+    }
+}
